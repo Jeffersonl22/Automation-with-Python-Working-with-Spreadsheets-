0858d59bdf7d5f9e76dd891a6668531ffe67f12b
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-06-16 16:46:20 -0300

    calculation for number per supplier

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ for product_row in range(2, product_list.max_row + 1):
     price = product_list.cell(product_row, 3).value
     product_number = product_list.cell(product_row, 1).value
     inventory_price = product_list.cell(product_row, 5)
+    
+    # calculation for number per supplier
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1    
