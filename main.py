008aee94966b44f73b16bcad703a60b4278e01ed
Jefferson Silva Lopes <84870965+Jeffersonl22@users.noreply.github.com>
2022-06-16 16:39:40 -0300

    defining the dictionares and a for loop
    Defining the dictionares and a for loop to acess the rows correctly.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-
 import openpyxl
 
 inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+total_value_per_supplier = {}
+products_under_10_inv = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_number = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
